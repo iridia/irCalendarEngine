ac2ac657c7a61114e9cfe8d96fe29e7d8b8a21c3
Evadne Wu <ev@monoceroi.com>
2010-07-06 02:19:13 +0800

    adds more delegate calls and finishes method-name-based handler method switching mechanism; moves portions of code over from arboreal.js

--- lib.iridia.calendarEngine.js
+++ lib.iridia.calendarEngine.js
@@ -41,7 +41,12 @@ window.iridia = (window && window.iridia || {})
 	iridia.calendarEngineDelegate = new JS.Interface([
 	
 		"calendarEngineDidLoad",
-		"calendarEngineDidReceiveEvents"
+		
+		"calendarEngineShouldSendRequest",
+		
+		"calendarEngineDidStartLoadingEvents",
+		"calendarEngineDidReceiveEvents",
+		"calendarEngineShouldRetry"
 	
 	]);
 
@@ -127,18 +132,24 @@ window.iridia = (window && window.iridia || {})
 			mono.log("Options: ", inOptions);
 		
 			this.setDelegate(inDelegate);
-			this.options = $.extend(true, inOptions, {
 			
-				calendarID: undefined,
-				initializeImmediately: true
+			if (this.delegate === undefined)
+			mono.info("Since there is no legit delegate this instance of irCalendarEngine will not even work.  Set one!");
 			
-			});
+			this.options = $.extend(true, {
+			
+				"calendarID": undefined,
+				"methodImmediatelyExecutes": true,
+				"methodName": "",
+				"methodArguments": {}
+			
+			}, inOptions);
 					
-			if (!!this.delegate) this.delegate.calendarEngineDidLoad(this);
+			this.delegate.calendarEngineDidLoad(this);
 			
-			if (!!this.options.initializeImmediately)
-			if (!!this.options.queryPredicate)
-			this.executeQuery(this.options.queryPredicate);
+			if (!!this.options.methodImmediatelyExecutes)
+			if (!!this.options.methodArguments)
+			this.executeQuery(this.options.methodName, this.options.methodArguments);
 			
 			mono.groupEnd();
 		
@@ -158,7 +169,7 @@ window.iridia = (window && window.iridia || {})
 			
 			} catch (exception) {
 			
-				return mono.die(mono.error("Object passed to iridia.calendarEngine does not conform to iridia.calendarEngineDelegate.  Bailing."));
+				return mono.die(mono.error(exception));
 			
 			}
 			
@@ -172,7 +183,7 @@ window.iridia = (window && window.iridia || {})
 	
 	//	Engine.
 		
-		executeQuery: function(queryHash) {
+		executeQuery: function(methodName, queryHash) {
 		
 			if (typeof queryHash != "object") return;
 			var finalQueryHash = {};
@@ -193,14 +204,135 @@ window.iridia = (window && window.iridia || {})
 				
 			});
 					
-			$.getJSON(iridia.calendarEngineBaseURLWithIdentifier(this.options.calendarID), finalQueryHash, this.processData);
+			$.jsonp({
+			
+				url: iridia.calendarEngineBaseURLWithIdentifier(this.options.calendarID),
+				
+				context: {
+				
+					engine: this,
+					method: methodName
+					
+				},
+				
+				data: finalQueryHash,				
+				callback: (function() {
+					
+					return "_jsonp" + ((Math.random() * (new Number(Math.pow(2, 4 * 8))))).toString(16) + ((Math.random() * (new Number(Math.pow(2, 4 * 8))))).toString(16);
+					
+				})(),
+				
+				pageCache: true,
+				
+				beforeSend: function (xOptions) {
+				
+					if (!xOptions.context.engine.delegate.calendarEngineShouldSendRequest()) {
+					
+						xOptions.abort();
+					
+					} else {
+					
+						xOptions.context.engine.delegate.calendarEngineDidStartLoadingEvents(xOptions.context.engine);
+					
+					}
+				
+				},
+				
+				success: (function(calendarEngineReference, successHandler, inMethodName) {
+					
+					return function(inJSONData, textStatus) {
+					
+						successHandler(calendarEngineReference, inMethodName, inJSONData, textStatus);
+						
+					}
+					
+				})(this, this.onRequestSuccess, methodName),
+				
+				complete: this.onRequestComplete
+				
+			});
 			
 		},
 		
-		processData: function(data) {
 		
-			mono.log("Calendar Engine shall process data", data);
+		
+		
+		
+	//	Request Handlers
+		
+		onRequestSuccess: function(calendarEngine, methodName, inJSONData, textStatus) {
+		
+		//	Since this handler is called by $.jsonp, we jump thru a few bounds to make it back to JS.Class
+		
+			if (typeof calendarEngine.processData[methodName] != "function") return;
+			calendarEngine.processData[methodName].call(calendarEngine, inJSONData);
 			
+		},
+		
+		onRequestComplete: function(xOptions, textStatus) {
+		
+			switch (textStatus) {
+			
+				case "error":
+				case "timeout":
+				
+					if (!xOptions.context.engine.delegate.calendarEngineShouldRetry(xOptions.context.engine)) return;
+					
+					$.jsonp(xOptions);
+					
+					break;
+
+				case "success":
+				default:
+
+					break;
+			
+			}
+			
+		},
+	
+	
+	
+	
+	
+	//	Processes
+		
+		processData: {
+		
+			"fetchEvents": function(inData) {
+				
+				var eventEntries = eval("inData.feed.entry");
+				if (eventEntries === undefined) return;
+				
+				var regulatedEventObjects = $.map(eventEntries, function(eventObject) {
+				
+					var eventTime = eventObject['gd$when'] && eventObject['gd$when'][0] || {};
+					var eventStartTime = eventTime && eventTime.startTime || "";
+					var eventStartDate = Date.fromISO8601(eventStartTime);
+				
+					return {
+					
+						title: eventObject.title && eventObject.title['$t'] || "",
+						content: eventObject.content && eventObject.content['$t'] || "",
+						startDate: eventStartDate,
+						link: eventObject.link
+					
+					};
+					
+				}).sort(function(firstObject, secondObject) {
+				
+				//	< 0: firstObject comes first
+				//	0: ordered same
+				//	> 0: secondObject comes first
+				
+					return (Number(firstObject && firstObject.startDate) - Number(secondObject && secondObject.startDate));
+					
+				});
+				
+				this.delegate.calendarEngineDidReceiveEvents(this, regulatedEventObjects);
+				
+			}
+		
 		}
 	
 	});
