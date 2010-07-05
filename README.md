18a42eb49ca1a17c6f7bb39e58c0b4b17ba1bf53
Evadne Wu <ev@monoceroi.com>
2010-07-05 20:56:15 +0800

    initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+#	`lib.iridia.calendarEngine.js`
+
+Minimalist Google Calendar API wrapper undergoing development.
+
+
+
+
---
+++ lib.iridia.calendarEngine.js
@@ -0,0 +1,41 @@
+//	lib.iridia.calendarEngine.js
+//	Evadne Wu at Iridia, 2010
+
+
+
+
+
+(function () {
+
+	if (!iridia) return;
+	if (!mono) return;
+	if (!jQuery) return;
+
+
+
+
+
+	iridia.calendarEngine = function (inOptions) {
+	
+		var options = $.extend(true, inOptions, {
+		
+			calendarID: undefined,
+			initializeImmediately: true
+		
+		});
+	
+	
+	
+	
+	
+	//	Workers
+		
+		var _executeAPIRequest = function () {};
+	
+	}
+
+})();
+
+
+
+
